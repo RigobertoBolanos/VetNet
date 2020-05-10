70461bb45a8e1108825aebae3fa57b882d402989
Juan Diego Carvajal Castaño <juandiego.carvajal@hotmail.com>
2020-05-10 18:34:21 -0500

    Some classes from models were completed

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -6,5 +6,93 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    //
+    private $pet, $veterinary, $service, $date;
+
+    public function __constructor($pet, $veterinary, $service, $date)
+    {
+        $this->pet = $pet ;
+        $this->veterinary = $veterinary ;
+        $this->service = $service ;
+        $this->date = $date ;
+    }
+
+    /**
+     * Get the value of date
+     */ 
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set the value of date
+     *
+     * @return  self
+     */ 
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of pet
+     */ 
+    public function getPet()
+    {
+        return $this->pet;
+    }
+
+    /**
+     * Set the value of pet
+     *
+     * @return  self
+     */ 
+    public function setPet($pet)
+    {
+        $this->pet = $pet;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of veterinary
+     */ 
+    public function getVeterinary()
+    {
+        return $this->veterinary;
+    }
+
+    /**
+     * Set the value of veterinary
+     *
+     * @return  self
+     */ 
+    public function setVeterinary($veterinary)
+    {
+        $this->veterinary = $veterinary;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of service
+     */ 
+    public function getService()
+    {
+        return $this->service;
+    }
+
+    /**
+     * Set the value of service
+     *
+     * @return  self
+     */ 
+    public function setService($service)
+    {
+        $this->service = $service;
+
+        return $this;
+    }
 }
--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,5 +6,51 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    private $name, $price;
+
+    public function __constructor($name, $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of price
+     */ 
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set the value of price
+     *
+     * @return  self
+     */ 
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
 }
